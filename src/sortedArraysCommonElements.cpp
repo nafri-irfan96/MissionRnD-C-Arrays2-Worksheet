297aa4f2e4c9764ae8a09bd21d6ceb4ed415423c
nafri-irfan96 <nafri-irfan96@missionrnd.com>
2016-01-26 00:14:04 +0530

    Submit Commit

--- src/sortedArraysCommonElements.cpp
+++ src/sortedArraysCommonElements.cpp
@@ -22,6 +22,119 @@ struct transaction {
 	char description[20];
 };
 
+
+int compare2(char date1[], char date2[]);
+int conversion(int *inp, int len);
+int binarySearch(struct transaction *Arr, int st, int end, char *date);
+
+
 struct transaction * sortedArraysCommonElements(struct transaction *A, int ALen, struct transaction *B, int BLen) {
-	return NULL;
+
+	if (A == NULL || B == NULL)
+		return NULL;
+
+	struct transaction *res = (struct transaction*)calloc(ALen, sizeof(struct transaction));
+
+	int i, j = 0;
+
+	for (i = 0; i < ALen; i++){
+
+		int flag = binarySearch(B, 0, BLen - 1, A[i].date);
+
+		if (flag >= 0){
+			res[j] = A[i];
+			j++;
+		}
+
+	}
+
+	if (j > 0)
+		return res;
+	else
+		return NULL;
+
+}
+
+
+int binarySearch(struct transaction *Arr, int st, int end, char *date){
+
+
+	if (st > end)
+		return -1;
+
+	int mid = (st + end) / 2;
+	int flag = (compare2(Arr[mid].date, date));
+	if (flag == 0)
+	{
+		return mid;
+	}
+	else  if (flag > 0)
+		return binarySearch(Arr, st, mid - 1, date);
+	else
+		return binarySearch(Arr, mid + 1, end, date);
+
+}
+
+int compare2(char date1[], char date2[]){
+
+	int dt1[3][4], dat2[3][4];
+
+	int i, j = 0, k = 0;
+
+	for (i = 0; date1[i] != '\0' && date2[i] != '\0'; i++){
+
+		if (date1[i] != '-' && date2[i] != '-'){
+			dt1[j][k] = (int)date1[i] - '0';
+			dat2[j][k] = (int)date2[i] - '0';
+			k++;
+		}
+		else
+		{
+			dt1[j][k] = dat2[j][k] = -1;
+			k = 0;
+			j++;
+		}
+
+	}
+
+	int y1, y2;
+	y1 = conversion(dt1[2], 4);
+	y2 = conversion(dat2[2], 4);
+
+
+	if (y1 != y2)
+		return y1 > y2 ? 1 : -1;
+	else
+	{
+
+		int mn1, mn2;
+		mn1 = conversion(dt1[1], 2);
+		mn2 = conversion(dat2[1], 2);
+
+		if (mn1 != mn2)
+			return mn1 > mn2 ? 1 : -1;
+		else
+		{
+			int day1, day2;
+			day1 = conversion(dt1[0], 2);
+			day2 = conversion(dat2[0], 2);
+			if (day1 != day2)
+				return day1 > day2 ? 1 : -1;
+			else
+				return 0;
+		}
+
+	}
+}
+
+int conversion(int inp[], int len){
+
+	int num = 0, i = 0;
+
+	while (i<len && inp[i] >= 0){
+		num = num * 10 + inp[i];
+		i++;
+	}
+
+	return num;
 }
